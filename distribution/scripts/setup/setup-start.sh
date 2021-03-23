01526e3529a1e74841d27c538d27b97144559994
SmithAbey <56927184+SmithAbey@users.noreply.github.com>
2021-03-23 10:06:51 +0530

    Update setup-start.sh

--- distribution/scripts/setup/setup-start.sh
+++ distribution/scripts/setup/setup-start.sh
@@ -41,35 +41,35 @@ mkdir $DOWNLOADS_DIR
 cd $HOME_DIR
 echo $GIT_USERNAME
 echo $GIT_PASSWORD
+# git push https://$GIT_USERNAME:$GIT_PASSWORD@github.com/SmithAbey/ballerina-performance-aws-ecs.git --all
+# Install required tools and packages
+source $SCRIPTS_DIR/setup/install-tools.sh
+source $SCRIPTS_DIR/docker/install-docker.sh
+source $SCRIPTS_DIR/java/install-java.sh
+source $SCRIPTS_DIR/ballerina/install-ballerina.sh
+source $SCRIPTS_DIR/jmeter/install-jmeter.sh
+
+# Build components package
+$SCRIPTS_DIR/setup/build-components.sh
+
+# Run Jmeter test
+chmod +x $SCRIPTS_DIR/jmeter/start-jmeter-test.sh
+source $SCRIPTS_DIR/jmeter/start-jmeter-test.sh $JMETER_OPTIONS
+
+# Create csv file
+cd $GITHUB_REPO_DIR/testresults
+sudo chmod +x create-summary-csv.sh
+sudo ./create-summary-csv.sh -n Passthrough HTTP service -x
+
+# Create markdown file
+echo "y" | apt-get install python3.8
+echo "y" | apt-get install -y python3-humanize
+sudo python3 create-summary-markdown.py --column-names "Scenario Name" "Heap Size" "Concurrent Users" "Message Size (Bytes)" "Back-end Service Delay (ms)" "Label" "# Samples" "Error Count" "Error %" "Throughput (Requests/sec)" "Average Response Time (ms)" "Average Users in the System" "Standard Deviation of Response Time (ms)" "Minimum Response Time (ms)" "75th Percentile of Response Time (ms)" "90th Percentile of Response Time (ms)" "95th Percentile of Response Time (ms)" "98th Percentile of Response Time (ms)" "99th Percentile of Response Time (ms)" "99.9th Percentile of Response Time (ms)" "Received (KB/sec)" "Sent (KB/sec)" --json-parameters parameters=/home/ubuntu/results/test-metadata.json
+
+# Push results to the git repo
+git add summary.md
+git add summary.csv
+git commit -m "Update performance results"
 git push https://$GIT_USERNAME:$GIT_PASSWORD@github.com/SmithAbey/ballerina-performance-aws-ecs.git --all
-# # Install required tools and packages
-# source $SCRIPTS_DIR/setup/install-tools.sh
-# source $SCRIPTS_DIR/docker/install-docker.sh
-# source $SCRIPTS_DIR/java/install-java.sh
-# source $SCRIPTS_DIR/ballerina/install-ballerina.sh
-# source $SCRIPTS_DIR/jmeter/install-jmeter.sh
-
-# # Build components package
-# $SCRIPTS_DIR/setup/build-components.sh
-
-# # Run Jmeter test
-# chmod +x $SCRIPTS_DIR/jmeter/start-jmeter-test.sh
-# source $SCRIPTS_DIR/jmeter/start-jmeter-test.sh $JMETER_OPTIONS
-
-# # Create csv file
-# cd $GITHUB_REPO_DIR/testresults
-# sudo chmod +x create-summary-csv.sh
-# sudo ./create-summary-csv.sh -n Passthrough HTTP service -x
-
-# # Create markdown file
-# echo "y" | apt-get install python3.8
-# echo "y" | apt-get install -y python3-humanize
-# sudo python3 create-summary-markdown.py --column-names "Scenario Name" "Heap Size" "Concurrent Users" "Message Size (Bytes)" "Back-end Service Delay (ms)" "Label" "# Samples" "Error Count" "Error %" "Throughput (Requests/sec)" "Average Response Time (ms)" "Average Users in the System" "Standard Deviation of Response Time (ms)" "Minimum Response Time (ms)" "75th Percentile of Response Time (ms)" "90th Percentile of Response Time (ms)" "95th Percentile of Response Time (ms)" "98th Percentile of Response Time (ms)" "99th Percentile of Response Time (ms)" "99.9th Percentile of Response Time (ms)" "Received (KB/sec)" "Sent (KB/sec)" --json-parameters parameters=/home/ubuntu/results/test-metadata.json
-
-# # Push results to the git repo
-# git add summary.md
-# git add summary.csv
-# git commit -m "Update performance results"
-
-# # Delete test stack and end the test
-# #aws cloudformation delete-stack --stack-name ec2-stack
+# Delete test stack and end the test
+#aws cloudformation delete-stack --stack-name ec2-stack
