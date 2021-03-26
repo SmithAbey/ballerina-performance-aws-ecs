f34e039b6bb9cb561f2ca6ff3ebe3f749d3a22cf
SmithAbey <56927184+SmithAbey@users.noreply.github.com>
2021-03-26 23:01:57 +0530

    Update start-jmeter-test.sh

--- distribution/scripts/jmeter/start-jmeter-test.sh
+++ distribution/scripts/jmeter/start-jmeter-test.sh
@@ -434,7 +434,6 @@ function test_scenarios() {
                     record_scenario_duration $scenario_name $current_execution_duration
                 done
             done
-            exit 1
             aws cloudformation delete-stack --stack-name ecs-stack
             aws cloudformation wait stack-delete-complete --stack-name ecs-stack
         done
